26fa4242697fbeb1198df7617312d0cbf903530e
j-seunghyun <jason5102@ajou.ac.kr>
2022-12-12 19:52:53 +0900

    get distance update

--- lamda/getDistance.py
+++ lamda/getDistance.py
@@ -20,8 +20,15 @@ def lambda_handler(event, context):
     data = table.scan()
     distance_list = data.get('Items')
     
-    current_distance_list = distance_list[0]
-    distance = current_distance_list.get('distance')
+    max_index = 0
+    distance = 0
+    
+    for current_distance in distance_list:
+        if current_distance.get('index') > max_index:
+            max_index = current_distance.get('index')
+            distance = current_distance.get('distance')
+    
+    print(distance)
     
         
     return {
