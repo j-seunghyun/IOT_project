6b7ec1d260570af9b3e4049ebf9c427caf7c9b8f
j-seunghyun <jason5102@ajou.ac.kr>
2022-12-12 20:15:18 +0900

    v_2022_12_12

--- distancetest.py
+++ distancetest.py
@@ -8,14 +8,22 @@ import base64
 
 postHostUrl = "https://0kyzkkrvkg.execute-api.ap-northeast-1.amazonaws.com/default/post/distance"
 
-async def post_distance(distance):
+async def post_distance(distance, current_index):
   #calc 함수에서 계산한 distance를 post
-  response = await requests.post(postHostUrl, headers=None)
+  datas = dict(index=current_index, distance = 200)
+  response = await requests.post(postHostUrl,data=datas,headers=None)
   data = json.loads(response.content)
-  #response = await requests.post(postHostUrl, data=distance, headers=None)
   return data
 
 def main():
   distance = 200
-  response = asyncio.run(post(distance))
-  print(json.dumps(response, ensure_ascii=False))
+  current_index = 0
+  #response = asyncio.run(post_distance(distance))
+  #print(json.dumps(response, ensure_ascii=False))
+
+  while True:
+    response = asyncio.run(post_distance(distance, current_index))
+    current_index = current_index+1
+    time.sleep(10)
+
+main()
